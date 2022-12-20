3e5de7a7da0cf8c96885deaa5aeb088a11cb61d8
Khudaija <86610468+Khudaija@users.noreply.github.com>
2022-12-20 20:13:30 +0530

    Update code preparation.java

--- code preparation.java
+++ code preparation.java
@@ -73,3 +73,23 @@ public class Main{
 
 ------------------------------------------------------
   
+// Reverse String
+
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        String rev = "";
+        String a = sc.nextLine();
+        char ch[] = a.toCharArray();
+        
+        for(int i=ch.length-1; i>=0; i--){
+            rev = rev + ch[i];
+        }
+        System.out.println("Reverse String: " + rev);
+    }
+}
+
+-------------------------------------------------------------------
+    
